c2ce644cd55608e287e7fcb816201cc83cdce942
Illia Dolzhenko <dolzhik@hotmail.com>
2024-08-08 19:20:03 +0100

    Fix group chat update processor

--- src/main/java/com/dolzhik/userbot/bot/updateProcessor/GroupChatUpdateProcessor.java
+++ src/main/java/com/dolzhik/userbot/bot/updateProcessor/GroupChatUpdateProcessor.java
@@ -17,47 +17,35 @@ public class GroupChatUpdateProcessor implements UpdateProcessor {
             var chatId = update.message.chatId;
             var text = Utills.getTextFromMessage(update.message).orElse(update.message.content.getClass().getName());
 
-            boolean repliedToMetion = false;
             if (update.message.containsUnreadMention && Utills.chance(settings.DIRECT_REPLY_CHANCE)) {
                 System.out.println("Chat " + chatId + " contains unread mention");
-                Optional.of(new Action(update, "reply"));
-                repliedToMetion = true;
+                return Optional.of(new Action(update, "reply"));
             }
 
-            boolean addedReaction = false;
-            if (Utills.chance(settings.REACTION_CHANCE) && !repliedToMetion) {
+            if (Utills.chance(settings.STICKER_CHANCE)) {
+                if (Utills.chance(0.5)) {
+                    System.out.println("Decided to reply with a sticker to the message: " + text);
+                    return Optional.of(new Action(update, "sticker"));
+                }
                 System.out.println("Decided to add a reaction to the message: " + text);
-                Optional.of(new Action(update, "reaction"));
-                addedReaction = true;
-            }
-
-            boolean repliedWithSticker = false;
-            if (Utills.chance(settings.STICKER_CHANCE) && !repliedToMetion && !addedReaction) {
-                System.out.println("Decided to reply with a sticker to the message: " + text);
-                Optional.of(new Action(update, "sticker"));
-                repliedWithSticker = true;
+                return Optional.of(new Action(update, "reaction"));
             }
 
             boolean newMessageSent = false;
-            if (Utills.chance(settings.NEW_MESSAGE_CHANCE) && !repliedToMetion && !repliedWithSticker) {
+            if (Utills.chance(settings.NEW_MESSAGE_CHANCE)) {
                 System.out.println("Decided to write a new message to chat: " + chatId);
-                Optional.of(new Action(update, "new"));
-                newMessageSent = true;
+                return Optional.of(new Action(update, "new"));
             }
 
-            boolean repliedNoMention = false;
-            if (Utills.chance(settings.REPLY_NO_MENTION_CHANCE) && !repliedToMetion && !newMessageSent) {
+            if (Utills.chance(settings.REPLY_NO_MENTION_CHANCE)) {
                 System.out.println("Decided to reply to the message: " + text);
-                Optional.of(new Action(update, "reply_no_mention"));
-                repliedNoMention = true;
+                return Optional.of(new Action(update, "reply_no_mention"));
             }
 
-            if (Utills.chance(settings.REPLY_TO_NAME_CHANCE) && !repliedToMetion && !newMessageSent
-                    && !repliedNoMention
-                    && text.toLowerCase().contains(settings.CHARACTER_NAME.toLowerCase().substring(0,
+            if (Utills.chance(settings.REPLY_TO_NAME_CHANCE) && text.toLowerCase().contains(settings.CHARACTER_NAME.toLowerCase().substring(0,
                             settings.CHARACTER_NAME.length() - 1))) {
                 System.out.println("Decided to reply to the message: " + text);
-                Optional.of(new Action(update, "reply_no_mention"));
+                return Optional.of(new Action(update, "reply_no_mention"));
             }
             return Optional.empty();
         }
